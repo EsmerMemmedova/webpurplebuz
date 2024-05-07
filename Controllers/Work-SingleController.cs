3d5f8312b6bc400568c63175fe0a575d588713a9
Esmer <bd7tqjs7c@code.edu.az>
2024-05-07 07:39:13 +0400

    webpurplebuz

---
+++ Controllers/Work-SingleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPurpleBuz.Controllers
+{
+    public class Work_SingleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
